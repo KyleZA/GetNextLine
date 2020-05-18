b5f709d61eb068c35bce81cb43e103f192ea1160
KyleZA <49956720+KyleZA@users.noreply.github.com>
2020-05-18 20:37:03 +0200

    changes made to GNL with comments added but no tests
    changes made to GNL with comments added but no tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/07 12:40:49 by kfrancis          #+#    #+#             */
-/*   Updated: 2020/05/18 18:56:20 by kfrancis         ###   ########.fr       */
+/*   Updated: 2020/05/18 20:34:06 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ static int	appendline(char **str, char **line)
 
 	len = 0;
 	while ((*str)[len] != '\n' && (*str)[len] != '\0')
-		len++;
+		len++; //read till end of string or new line
 	if ((*str)[len] == '\n')
 	{
-		*line = ft_strsub(*str, 0, len);
-		temp = ft_strdup(&((*str)[len + 1]));
-		free(*str);
-		*str = temp;
-		if ((*str)[0] == '\0')
+		*line = ft_strsub(*str, 0, len); // set *line to the read string until len bytes
+		temp = ft_strdup(&((*str)[len + 1])); // set temp to whole of str including the newiline
+		free(*str); //free data within str.
+		*str = temp; //now set str to temps data
+		if ((*str)[0] == '\0') 
 			ft_strdel(str);
 	}
 	else
@@ -40,11 +40,11 @@ static int	appendline(char **str, char **line)
 static int	output(char **str, char **line, int ret, int fd)
 {
 	if (ret < 0)
-		return (-1);
+		return (-1); // return if error found
 	else if (ret == 0 && str[fd] == NULL)
-		return (0);
+		return (0); // return if reaches end of file descriptor
 	else
-		return (appendline(&str[fd], line));
+		return (appendline(&str[fd], line)); // run appendline function if line is read
 }
 
 int get_next_line(const int fd, char **line)
@@ -55,20 +55,20 @@ int get_next_line(const int fd, char **line)
 	char		*temp;
 
 	if (fd < 0 || line == NULL)
-		return (-1);
-	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
+		return (-1); // checks to see if file descriptor is empty or non existant.
+	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0) // sets ret value to 9 but reads buffer size of 8 (size of BUFF_SIZE)
 	{
-		buffer[ret] = '\0';
+		buffer[ret] = '\0'; // sets last character in string to end of line character
 		if (str[fd] == NULL)
-			str[fd] = ft_strdup(buffer);
+			str[fd] = ft_strdup(buffer); // duplicates buffer info into str[fd]
 		else
 		{
-			temp = ft_strjoin(str[fd], buffer);
-			free(str[fd]);
-			str[fd] = temp;
+			temp = ft_strjoin(str[fd], buffer); // while str[fd] is not empty adds buffer at end of whatever is in str[fd]
+			free(str[fd]); // frees any data str[fd] was holding
+			str[fd] = temp; // sets str[fd] to new and all the data within str[fd]
 		}
 		if (ft_strchr(str[fd], '\n'))
-			break ;
+			break ; // if find new line is found exit loop.
 	}
-	return (output(str, line, ret, fd));
+	return (output(str, line, ret, fd)); // run output function
 }
