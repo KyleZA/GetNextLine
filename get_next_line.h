e37f84fc0685eafa2c061bbefcdf7a2d32973d72
KyleZA <49956720+KyleZA@users.noreply.github.com>
2020-05-18 19:02:39 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
-# include <fcntl.h>
-
-# define BUFF_SIZE 8
+# define BUFF_SIZE 1
 # define FD_SIZE 4096
 
-# include <stdlib.h>
-# include <stdio.h>
 # include <fcntl.h>
-# include <sys/types.h>
-# include <sys/uio.h>
-# include <unistd.h>
 # include "libft/libft.h"
 
 int				get_next_line(int const fd, char **line);
